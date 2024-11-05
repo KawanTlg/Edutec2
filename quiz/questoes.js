2767f38fca84c96c95d8e2b0b45b153df54d3cf4
Kawan Ribeiro <kawanribeiro.ph2@gmail.com>
2024-11-05 20:38:34 +0000

    tabela centralizada

---
+++ quiz/questoes.js
@@ -0,0 +1,20 @@
+export const questoes = [
+    {
+      questao: "Quem criou o primeiro modelo atômico?",
+      respostas: [
+        { text: "Thomsom", correct: false },
+        { text: "Rutherford", correct: false },
+        { text: "Bohr", correct: false },
+        { text: "Dalton", correct: true }
+      ]
+    },
+    {
+      questao: "Quem criou o modelo da bola de bilhar?",
+      respostas: [
+        { text: "Dalton", correct: true },
+        { text: "Thomsom", correct: false },
+        { text: "Rutherford", correct: false },
+        { text: "Bohr", correct: false }
+      ]
+    }
+  ];
